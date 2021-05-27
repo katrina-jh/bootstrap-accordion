66f90b00f57aad2821c3f89c03de117006aac83e
Katrina H <katrina.hitchcock@planetark.com>
2021-05-27 14:33:46 +1000

    use for each instead of for loop

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const checkboxes = document.querySelectorAll('.form-check-input');
 const textarea = document.querySelector('textarea');
 
-for (let i = 0; i < checkboxes.length; i++) {
-  checkboxes[i].addEventListener('click', function () {
-    if (checkboxes[i].checked) {
-      console.log(checkboxes[i].value);
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener('click', function () {
+    if (checkbox.checked) {
+      console.log(checkbox.value);
     } else {
       console.log('un-checked');
     }
   });
-}
+});
 
 textarea.addEventListener('keypress', function () {
   console.log(textarea.value);
