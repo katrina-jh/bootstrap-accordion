d4e2e91a20f0d64e72bf446216fdca0a5eda70fd
Katrina H <katrina.hitchcock@planetark.com>
2021-05-27 12:56:22 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const checkboxes = document.getElementsByClassName('form-check-input');
+const textarea = document.querySelector('textarea');
+
+for (let i = 0; i < checkboxes.length; i++) {
+  checkboxes[i].addEventListener('click', function () {
+    if (checkboxes[i].checked) {
+      console.log(checkboxes[i].value);
+    } else {
+      console.log('un-checked');
+    }
+  });
+}
+
+textarea.addEventListener('keypress', function () {
+  console.log(textarea.value);
+});
